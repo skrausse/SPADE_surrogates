7f52b0f9252a8cf718299062b8fda61a17126ebd
stellalessandra <a.stella@fz-juelich.de>
2021-01-18 10:44:27 +0100

    added configfile and generation concatenated data script

---
+++ code/generate_original_concatenated_data.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import quantities as pq
+import yaml
+from yaml import Loader
+import rgutils
+
+if __name__ == '__main__':
+    with open("configfile.yaml", 'r') as stream:
+        config = yaml.load(stream, Loader=Loader)
+
+    sessions = config['sessions']
+    epochs = config['epochs']
+    trialtypes = config['trialtypes']
+    winlen = config['winlen']
+    unit = config['unit']
+    binsize = (config['binsize'] * pq.s).rescale(unit)
+    firing_rate_threshold = config['firing_rate_threshold']
+    seed = config['seed']
+    SNR_thresh = config['SNR_thresh']
+    synchsize = config['synchsize']
+    sep = 2 * winlen * binsize
+
+    for session in sessions:
+        if not os.path.exists(f'../data/concatenated_spiketrains/{session}'):
+            os.makedirs(f'../data/concatenated_spiketrains/{session}')
+        for epoch in epochs:
+            for trialtype in trialtypes:
+                print(f'Loading data {session} {epoch} {trialtype}')
+                sts = rgutils.load_epoch_concatenated_trials(
+                    session,
+                    epoch,
+                    trialtypes=trialtype,
+                    SNRthresh=SNR_thresh,
+                    synchsize=synchsize,
+                    sep=sep,
+                    firing_rate_threshold=firing_rate_threshold)
+                np.save(f'../data/concatenated_spiketrains/{session}/'
+                        f'{epoch}_{trialtype}.npy',
+                        sts)
