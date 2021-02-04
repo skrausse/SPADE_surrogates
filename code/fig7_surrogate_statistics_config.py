89160cfdc1acf128f271270c981d53cff64a91a8
pbouss <peter.bouss@gmail.com>
2021-02-04 13:03:26 +0100

    updated surrogates generation

--- code/fig7_surrogate_statistics_config.py
+++ code/fig7_surrogate_statistics_config.py
@@ -1,4 +1,6 @@
 import os
+
+import numpy as np
 import quantities as pq
 
 from scipy.special import gamma as gamma_function
@@ -12,14 +14,51 @@ if not os.path.exists(DATA_PATH):
 if not os.path.exists(PLOT_PATH):
     os.makedirs(PLOT_PATH)
 
+FIG_NAME = 'Fig7_surrogate_statistics.eps'
+
 #  data generation
 
 # Global surrogate methods and spike train types.
-SURR_METHODS = ('UD', 'UDR', 'ISI-D', 'JISI-D', 'SHIFT-ST', 'BIN-SHUFF')
+SURR_METHODS = ('UD', 'UDD', 'ISI-D', 'JISI-D', 'SHIFT-ST', 'BIN-SHUFF')
 DATA_TYPES = ('Poisson', 'PPD', 'Gamma')
+
+# Data type for firing rate step and for eff. moved
+STEP_DATA_TYPE = 'Gamma'
+
+# firing rate for ISI-distribtion, autocorrelation, cross-correlation,
+# cv-change
+FIRING_RATE = 60.*pq.Hz
+# rates for clipped firing rates and eff. moved
+RATES = np.arange(10., 100.1, 10.) * pq.Hz
+# Firing rates for step
+FIRING_RATES_STEP = (10 * pq.Hz, 80 * pq.Hz)
+# DURATION_RATES_STEP
+DURATION_RATES_STEP = 150. * pq.ms
+# number of spikes for ISI-distribtion, autocorrelation, cross-correlation
+HIGH_NUMBER_SPIKES = 500000
+# number of spikes for clipped rates, effective moved, CV change
+LOW_NUMBER_SPIKES = 10000
+# number spiketrains for firing rate step
+NUMBER_SPIKETRAINS = 10000
+
+# CVS for CV change
+CVS = np.arange(0.4, 1.21, 0.05)
 
 # dither-parameter
 DITHER = 25 * pq.ms
+# SPADE bin size
+SPADE_BIN_SIZE = 5. * pq.ms
+
+# bin size for ISI-distribtion, autocorrelation, cross-correlation
+BIN_SIZE = 1. * pq.ms
+# number of bins auto-/cross-correlation
+NUM_BINS = 60
+# ISI limit in terms of mean ISI
+ISI_LIM = 3.
+
+# Parameters Trial shifting
+TRIAL_LENGTH = 500. * pq.ms
+TRIAL_SEPARATION = 0. * pq.ms
 
 # Values for refractory period and CV2
 DEAD_TIME = 1.6 * pq.ms
@@ -64,14 +103,16 @@ SHAPE_FACTOR = get_shape_factor_from_cv2(CV2)
 
 #  plotting
 
+FIGSIZE = (7.5, 8.75)
 XLABELPAD = -0.5
 YLABELPAD = -0.5
 ORIGINAL_LINEWIDTH = 2.2
-
+SURROGATES_LINEWIDTH = 0.75
+FONTSIZE = 10
 
 LABELS = {'original': 'original',
           'UD': 'UD',
-          'UDR': 'UDD',
+          'UDD': 'UDD',
           'ISI-D': 'ISI-D',
           'JISI-D': 'JISI-D',
           'SHIFT-ST': 'TR-SHIFT',
@@ -79,7 +120,7 @@ LABELS = {'original': 'original',
 
 COLORS = {'original': 'C0',
           'UD': 'C1',
-          'UDR': 'C2',
+          'UDD': 'C2',
           'ISI-D': 'C4',
           'JISI-D': 'C6',
           'SHIFT-ST': 'C3',
@@ -87,7 +128,7 @@ COLORS = {'original': 'C0',
 
 LINE_STYLES = {'original': 'solid',
                'UD': 'solid',
-               'UDR': 'solid',
+               'UDD': 'solid',
                'ISI-D': 'dashed',
                'JISI-D': 'solid',
                'SHIFT-ST': 'solid',
@@ -95,3 +136,12 @@ LINE_STYLES = {'original': 'solid',
 
 # Panel letters
 LETTERS = ('A', 'B', 'C', 'D', 'E', 'F')
+
+# limits AC/CC relative to rate
+AC_BOTTOM = 0.8
+AC_TOP = 1.1
+CC_BOTTOM = 0.8
+CC_TOP = 1.6
+
+# AC/ CC xlim relative to dither
+AC_CC_XLIM = 3.05
--- code/fig7_surrogate_statistics_plot.py
+++ code/fig7_surrogate_statistics_plot.py
@@ -2,27 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import quantities as pq
 
-import fig7_surrogate_statistics_config as cfg
+import fig7_surrogate_statistics_config as cf
 
-DATA_PATH = cfg.DATA_PATH
-PLOT_PATH = cfg.PLOT_PATH
+DATA_PATH = cf.DATA_PATH
+PLOT_PATH = cf.PLOT_PATH
 
-SURR_METHODS = cfg.SURR_METHODS
-DATA_TYPES = cfg.DATA_TYPES
+FIG_NAME = cf.FIG_NAME
 
-XLABELPAD = cfg.XLABELPAD
-YLABELPAD = cfg.YLABELPAD
-ORIGINAL_LINEWIDTH = cfg.ORIGINAL_LINEWIDTH
+SURR_METHODS = cf.SURR_METHODS
+DATA_TYPES = cf.DATA_TYPES
 
-LABELS = cfg.LABELS
-COLORS = cfg.COLORS
+STEP_DATA_TYPE = cf.STEP_DATA_TYPE
 
-LINE_STYLES = cfg.LINE_STYLES
+FIRING_RATE = cf.FIRING_RATE
 
-LETTERS = cfg.LETTERS
+DURATION_RATES_STEP = cf.DURATION_RATES_STEP
+
+DITHER = cf.DITHER
+
+FIGSIZE = cf.FIGSIZE
+XLABELPAD = cf.XLABELPAD
+YLABELPAD = cf.YLABELPAD
+ORIGINAL_LINEWIDTH = cf.ORIGINAL_LINEWIDTH
+SURROGATES_LINEWIDTH = cf.SURROGATES_LINEWIDTH
+FONTSIZE = cf.FONTSIZE
+
+LABELS = cf.LABELS
+COLORS = cf.COLORS
+
+LINE_STYLES = cf.LINE_STYLES
+
+LETTERS = cf.LETTERS
+
+AC_BOTTOM = cf.AC_BOTTOM
+AC_TOP = cf.AC_TOP
+CC_BOTTOM = cf.CC_BOTTOM
+CC_TOP = cf.CC_TOP
+
+AC_CC_XLIM = cf.AC_CC_XLIM
 
 
-def plot_clipped_firing_rate_and_eff_moved(axes_clip, axes_sync=None):
+def plot_clipped_firing_rate_and_eff_moved(axes_clip):
     """
     This function makes a plot for the clipped firing rate,
     the ratio of spike in same bins and the ratio
@@ -40,45 +60,24 @@ def plot_clipped_firing_rate_and_eff_moved(axes_clip, axes_sync=None):
     ratio_clipped = results['ratio_clipped']
     ratio_clipped_surr = results['ratio_clipped_surr']
 
-    ratio_indep_moved = results['ratio_indep_moved']
-    ratio_moved = results['ratio_moved']
-
     for type_id, data_type in enumerate(DATA_TYPES):
         axes_clip[type_id].plot(
-            rates[type_id], 1. - np.array(ratio_clipped[type_id]),
+            rates[data_type], 1. - np.array(ratio_clipped[data_type]),
             label='original',
             linewidth=ORIGINAL_LINEWIDTH,
             color=COLORS['original'],
             linestyle=LINE_STYLES['original']
         )
-        if axes_sync is not None:
-            axes_sync[type_id].plot(
-                rates[type_id], ratio_indep_moved[type_id],
-                label='indep.',
-                linewidth=ORIGINAL_LINEWIDTH,
-                linestyle=LINE_STYLES['original']
-            )
         for surr_method in SURR_METHODS:
             axes_clip[type_id].plot(
-                rates[type_id],
-                1. - np.array(ratio_clipped_surr[type_id][surr_method]),
+                rates[data_type],
+                1. - np.array(ratio_clipped_surr[data_type][surr_method]),
                 label=surr_method,
                 color=COLORS[surr_method],
                 linestyle=LINE_STYLES[surr_method])
-            if axes_sync is not None:
-                axes_sync[type_id].plot(
-                    rates[type_id],
-                    ratio_moved[type_id][surr_method],
-                    label=surr_method,
-                    color=COLORS[surr_method],
-                    linestyle=LINE_STYLES[surr_method])
 
         axes_clip[type_id].set_xlabel(r'$\lambda$ in Hz',
                                       labelpad=XLABELPAD)
-
-        if axes_sync is not None:
-            axes_sync[type_id].set_xlabel(r'$\lambda$ in Hz',
-                                          labelpad=XLABELPAD)
 
 
 def _plot_isi(axes_isi, data_type, type_id, surr_method):
@@ -120,25 +119,24 @@ def _plot_ac_cc(axes_ac, axes_cc, type_id, data_type,
             linestyle=LINE_STYLES[surr_method])
 
 
-def _label_axes(axes_isi, axes_ac, axes_cc,
-                dither, rate):
-    for type_id, data_type in enumerate(DATA_TYPES):
-        for analysis_id, axis in enumerate(
-                (axes_isi[type_id], axes_cc[type_id])):
+def _label_axes(axes_isi, axes_ac, axes_cc):
+    for axis_isi, axis_cc in zip(axes_isi, axes_cc):
+        for axis in (axis_isi, axis_cc):
             axis.set_xlabel(r'$\tau$ in ms', labelpad=XLABELPAD)
 
-        axes_ac[type_id].set_ylim(bottom=0.8 * rate,
-                                  top=1.1 * rate)
-        axes_cc[type_id].set_ylim(bottom=0.8 * rate,
-                                  top=1.6 * rate)
-        axes_ac[type_id].set_xlim(left=-3.05 * dither, right=3.05 * dither)
-
-        axes_cc[type_id].set_xlim(left=-3.05 * dither, right=3.05 * dither)
+    for axis_ac, axis_cc in zip(axes_ac, axes_cc):
+        axis_ac.set_ylim(bottom=AC_BOTTOM * FIRING_RATE,
+                         top=AC_TOP * FIRING_RATE)
+        axis_cc.set_ylim(bottom=CC_BOTTOM * FIRING_RATE,
+                         top=CC_TOP * FIRING_RATE)
+        axis_ac.set_xlim(left=-AC_CC_XLIM * DITHER,
+                         right=AC_CC_XLIM * DITHER)
+        axis_cc.set_xlim(left=-AC_CC_XLIM * DITHER,
+                         right=AC_CC_XLIM * DITHER)
 
 
 def plot_statistical_analysis_of_single_rate(
-        axes_isi, axes_ac, axes_cc, dither=15. * pq.ms,
-        rate=50 * pq.Hz):
+        axes_isi, axes_ac, axes_cc):
     for type_id, data_type in enumerate(DATA_TYPES):
         _plot_isi(axes_isi, data_type, type_id, surr_method='original')
         _plot_ac_cc(axes_ac, axes_cc, type_id, data_type,
@@ -150,11 +148,10 @@ def plot_statistical_analysis_of_single_rate(
             _plot_ac_cc(axes_ac, axes_cc, type_id, data_type,
                         surr_method)
 
-    _label_axes(axes_isi, axes_ac, axes_cc,
-                dither, rate)
+    _label_axes(axes_isi, axes_ac, axes_cc)
 
 
-def plot_firing_rate_change(axis, data_type='Gamma'):
+def plot_firing_rate_change(axis):
     """
     This function creates a plot which shows the change in firing rate profile
     after applying a surrogate method. Starting point is a process (either PPR
@@ -171,13 +168,13 @@ def plot_firing_rate_change(axis, data_type='Gamma'):
     """
     results = np.load(f'{DATA_PATH}/rate_step.npy', allow_pickle=True).item()
 
-    rate_original = results[data_type]['original']
+    rate_original = results['original']
     axis.plot(rate_original.times, rate_original.simplified.magnitude,
               label='original', linewidth=ORIGINAL_LINEWIDTH,
               color=COLORS['original'], linestyle=LINE_STYLES['original'])
 
     for surr_method in SURR_METHODS:
-        rate_dithered = results[data_type][surr_method]
+        rate_dithered = results[surr_method]
 
         axis.plot(rate_dithered.times, rate_dithered.simplified.magnitude,
                   label=surr_method, color=COLORS[surr_method],
@@ -186,10 +183,11 @@ def plot_firing_rate_change(axis, data_type='Gamma'):
     axis.set_xlabel(r't in ms', labelpad=XLABELPAD)
     axis.set_ylabel(r'$\lambda(t)$ in Hz', labelpad=YLABELPAD)
 
-    axis.set_xticks([0., 50., 100., 150.])
+    t_stop = DURATION_RATES_STEP.rescale(pq.ms).magnitude
+    axis.set_xticks([0., 1/3 * t_stop, 2/3 * t_stop, t_stop])
 
 
-def plot_eff_moved(axis, data_type='Gamma'):
+def plot_eff_moved(axis):
     """
     This function makes a plot for the clipped firing rate,
     the ratio of spike in same bins and the ratio
@@ -208,21 +206,19 @@ def plot_eff_moved(axis, data_type='Gamma'):
     ratio_indep_moved = results['ratio_indep_moved']
     ratio_moved = results['ratio_moved']
 
-    for type_id, dt_type in enumerate(DATA_TYPES):
-        if dt_type == data_type:
-            axis.plot(
-                rates[type_id], ratio_indep_moved[type_id],
-                label='indep.',
-                linewidth=ORIGINAL_LINEWIDTH,
-                color=COLORS['original'],
-                linestyle=LINE_STYLES['original'])
-            for surr_method in SURR_METHODS:
-                axis.plot(
-                    rates[type_id],
-                    ratio_moved[type_id][surr_method],
-                    label=surr_method,
-                    color=COLORS[surr_method],
-                    linestyle=LINE_STYLES[surr_method])
+    axis.plot(
+        rates[STEP_DATA_TYPE], ratio_indep_moved[STEP_DATA_TYPE],
+        label='indep.',
+        linewidth=ORIGINAL_LINEWIDTH,
+        color=COLORS['original'],
+        linestyle=LINE_STYLES['original'])
+    for surr_method in SURR_METHODS:
+        axis.plot(
+            rates[STEP_DATA_TYPE],
+            ratio_moved[STEP_DATA_TYPE][surr_method],
+            label=surr_method,
+            color=COLORS[surr_method],
+            linestyle=LINE_STYLES[surr_method])
 
     axis.set_xlabel(r'$\lambda$ in Hz', labelpad=XLABELPAD)
     axis.set_ylabel(r'$N_{moved}/N$', labelpad=YLABELPAD)
@@ -261,13 +257,12 @@ def _dash_spines(axis):
 
 
 def plot_statistics_overview():
-    plt.rcParams.update({'font.size': 10,
+    plt.rcParams.update({'font.size': FONTSIZE,
                          'text.usetex': True,
-                         'lines.linewidth': 0.75,
-                         'figure.figsize': (7.5, 8.75)})
+                         'lines.linewidth': SURROGATES_LINEWIDTH,
+                         'figure.figsize': FIGSIZE})
     start_down = 0.05
     height_figure = 0.12
-    # epsilon = 0.035
     epsilon = 0.05
     delta_down = 0.05 + height_figure + epsilon
     delta_up = 0.05
@@ -286,11 +281,6 @@ def plot_statistics_overview():
                   (1-(delta_down+delta_up)-2*epsilon)/4])
         for x in range(3)] for y in range(4)]
 
-    # for axis in axes:
-    #    for ax in axis:
-    #        ax.grid()
-    #        _dash_spines(ax)
-
     lower_axes = \
         [fig.add_axes(
             rect=[left_move + delta_down
@@ -301,14 +291,6 @@ def plot_statistics_overview():
                   height_figure])
          for x in range(3)]
 
-    # for ax in lower_axes:
-    #    ax.grid()
-    #    _dash_spines(ax)
-
-    # [left, bottom, width, height]
-
-    # gs = fig.add_gridspec(4, 3, hspace=0, wspace=0)
-    # axes = gs.subplots(sharey='row')
     axes_clip, axes_isi, axes_ac, axes_cc = axes
     axis_step, axis_moved, axis_cv = lower_axes
 
@@ -320,15 +302,14 @@ def plot_statistics_overview():
     plot_clipped_firing_rate_and_eff_moved(axes_clip)
 
     plot_statistical_analysis_of_single_rate(
-        axes_isi, axes_ac, axes_cc, rate=60*pq.Hz)
+        axes_isi, axes_ac, axes_cc)
 
     plot_firing_rate_change(axis_step)
     plot_eff_moved(axis_moved)
     plot_cv_change(axis_cv)
 
     for data_id, data_type in enumerate(DATA_TYPES):
-        subtitle = data_type
-        axes[0][data_id].set_title(subtitle)
+        axes[0][data_id].set_title(data_type)
 
     for axis_id, axis in enumerate(axes):
         if axis_id < 3:
@@ -346,28 +327,27 @@ def plot_statistics_overview():
     for axis in axes_ac:
         _hide_x_ticks(axis)
 
-    for axis in axes:
-        ylims = [axis[x].get_ylim() for x in range(3)]
-        ylim = (min(ylims[x][0] for x in range(3)),
-                max(ylims[x][1] for x in range(3)))
-        [axis[x].set_ylim(ylim) for x in range(3)]
+    for axes_in_row in axes:
+        ylims = [axis.get_ylim() for axis in axes_in_row]
+        ylim = (min(ylim[0] for ylim in ylims),
+                max(ylim[1] for ylim in ylims))
 
-    # fig.suptitle('Surrogate Statistics', y=0.96)
+        for axis in axes_in_row:
+            axis.set_ylim(ylim)
+
     handles, labels = axes_isi[0].get_legend_handles_labels()
-    x, y = 2, 0
+    pos_x, pos_y = 2, 0
     legend_loc = (
-        left_move + delta_down + (1-(delta_down+delta_up))/3*x
+        left_move + delta_down + (1-(delta_down+delta_up))/3*pos_x
         + (1-(delta_down+delta_up))/3 + 0.003,
-        delta_down + (1-(delta_down+delta_up)-2*epsilon)/4*(3-y)
-        + epsilons[y] + 0.02)  # loc=(x, y)
+        delta_down + (1-(delta_down+delta_up)-2*epsilon)/4*(3-pos_y)
+        + epsilons[pos_y] + 0.02)  # loc=(x, y)
     legend = fig.legend(handles, labels, fontsize=8., loc=legend_loc)
     frame = legend.get_frame()
     frame.set_facecolor('0.9')
     frame.set_edgecolor('0.9')
 
-    fig.savefig(f'{PLOT_PATH}/'
-                f'Fig_surrogate_statistics.eps',
-                dpi=300)
+    fig.savefig(f'{PLOT_PATH}/{FIG_NAME}', dpi=300)
 
 
 if __name__ == '__main__':
