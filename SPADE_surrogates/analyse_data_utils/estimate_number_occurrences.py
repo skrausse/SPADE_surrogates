0810cc8b01644dfd27fe530e12a2127d3c93de0f
pbouss <peter.bouss@gmail.com>
2021-08-03 13:34:01 +0200

    finished tidying up estimate number occurrences

--- SPADE_surrogates/analyse_data_utils/estimate_number_occurrences.py
+++ SPADE_surrogates/analyse_data_utils/estimate_number_occurrences.py
@@ -1,10 +1,16 @@
-import numpy as np
+import os
 import math
 import copy
+
+import numpy as np
 from scipy.stats import binom
 from scipy.special import binom as binom_coeff
+
 import quantities as pq
-import os
+
+import yaml
+from yaml import Loader
+
 from SPADE_surrogates.analyse_data_utils import spade_utils as utils
 
 
@@ -419,3 +425,72 @@ def estimate_number_occurrences(
     if not os.path.exists('./excluded_neurons.npy'):
         np.save('excluded_neurons', excluded_neurons)
     return param_dict, excluded_neurons
+
+
+def execute_estimate_number_of_occurrences(original=True):
+    with open("../configfile.yaml", 'r') as stream:
+        config = yaml.load(stream, Loader=Loader)
+    # The 5 epochs to analyze
+    epochs = config['epochs']
+    # The 4 trial types to analyze
+    trialtypes = config['trialtypes']
+    # The sessions to analyze
+    sessions = config['sessions']
+    # Absolute minimum number of occurrences of a pattern
+    abs_min_occ = config['abs_min_occ']
+    # Magnitude of the binsize used
+    binsize = config['binsize']
+    # The percentile for the Poisson distribution to fix minimum number of occ
+    percentile_poiss = config['percentile_poiss']
+    # The percentile for the Poisson distribution of rates
+    percentile_rates = config['percentile_rates']
+    # minimum number of spikes per patterns
+    abs_min_spikes = config['abs_min_spikes']
+    # The winlen parameter for the SPADE analysis
+    winlen = config['winlen']
+    # Spectrum to use
+    spectrum = config['spectrum']
+    # Dithering to use to generate surrogates in seconds
+    dither = config['dither']
+    # Number of surrogates to generate
+    n_surr = config['n_surr']
+    # Significance level
+    alpha = config['alpha']
+    # Multitesting statistical correction
+    correction = config['correction']
+    # PSR parameters
+    psr_param = config['psr_param']
+    # Unit in which every time of the analysis is expressed
+    unit = config['unit']
+    # Firing rate threshold to possibly exclude neurons
+    firing_rate_threshold = config['firing_rate_threshold']
+    # Surrogate method to use
+    surr_method = config['surr_method']
+
+    if original:
+        processes = ('original', )
+    else:
+        # Data being generated
+        processes = config['processes']
+
+    # loading parameters
+    estimate_number_occurrences(
+        sessions=sessions,
+        epochs=epochs,
+        trialtypes=trialtypes,
+        binsize=binsize,
+        abs_min_spikes=abs_min_spikes,
+        abs_min_occ=abs_min_occ,
+        correction=correction,
+        psr_param=psr_param,
+        alpha=alpha,
+        n_surr=n_surr,
+        dither=dither,
+        spectrum=spectrum,
+        winlen=winlen,
+        percentile_poiss=percentile_poiss,
+        percentile_rates=percentile_rates,
+        unit=unit,
+        processes=processes,
+        firing_rate_threshold=firing_rate_threshold,
+        surr_method=surr_method)
