539b91c277787588806eb12106a7de5bd32c6d9f
Sven Krausse <s.krausse@fz-juelich.de>
2023-07-11 07:44:48 +0200

    Adapt for front-ng cluster

--- SPADE_surrogates/analysis_experimental_data/snakejob.sh
+++ SPADE_surrogates/analysis_experimental_data/snakejob.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=FAIL								                                # notify on job failed
 #SBATCH --output=../../../01_cluster_output/surrogates_%j.out                           # redirect cluster output
 #SBATCH --error=../../../01_cluster_output/surrogates_%j.err                            # redirect cluster errors
-#SBATCH --partition=hamstein                                                            # Specify which partition to use
+#SBATCH --partition=hamstein2022                                                        # Specify which partition to use
 
 module load mpi/openmpi/4.0.3rc4
 
