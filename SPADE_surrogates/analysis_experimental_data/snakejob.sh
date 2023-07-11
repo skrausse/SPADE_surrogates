318320711779e58e459c6c05be12ca52ba08aa75
Sven Krausse <s.krausse@fz-juelich.de>
2023-07-11 07:34:47 +0200

    update cluster running routine

--- SPADE_surrogates/analysis_experimental_data/snakejob.sh
+++ SPADE_surrogates/analysis_experimental_data/snakejob.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#SBATCH --time=24:00:00   								                                # walltime
-#SBATCH --ntasks=60   									                                # number of processor cores (i.e. tasks)
+#SBATCH --time=96:00:00   								                                # walltime
 #SBATCH --mem=32G	   								                                    # memory per CPU core
 #SBATCH -J "SPADE surrogates"                                                           # job name
 #SBATCH --mail-user=s.krausse@fz-juelich.de   						                    # email address
@@ -10,24 +9,27 @@
 #SBATCH --error=../../../01_cluster_output/surrogates_%j.err                            # redirect cluster errors
 #SBATCH --partition=hamstein                                                            # Specify which partition to use
 
-cd ~/projects/SPADE_surrogates/SPADE_surrogates/analysis_experimental_data
-module load mpi/openmpi
-module load mpi/mpich/3.2
-module load mpi/mpich/3.3.2
+module load mpi/openmpi/4.0.3rc4
 
-snakemake   --cores 60 \
-            --rerun-incomplete  \
-            --keep-going \
-            --latency-wait 90 \
-            --nolock \
+# source activate patterns
 
-scontrol show jobid ${SLURM_JOBID} -dd                                                  # Job summary at exit
+snakemake --unlock\
+	  --configfile configfile.yaml
+          --use-conda\
+          --cores 1
 
-# --jobs                maximum number of CPU cores/jobs use in parallel
-# --cluster             cluster command to submit rules as jobs
-# --cluster-config      file to specify cluster wildcards
-# --jobname             name of snakemake submited jobs
-# --latency-wait        outwait filesystem latency after jobs are finished (s)
-# --keep-going          go on with independet jobs if a job fails
-# --rerun-incomplete    rerun jobs where the output is incomplete
-# "SBATCH --nodes 2" would fix the required nodes
+snakemake --jobs 1000\
+          --cluster-config cluster.json\
+          --cluster "sbatch -n {cluster.n}\
+                            --time {cluster.time}\
+                            --mail-type=FAIL\
+                            --mem={cluster.mem}\
+	                    --partition=blaustein\
+	                    -o /users/krausse/spade_comparison/cluster_output/rule-%j.out\
+                        -e /users/krausse/spade_comparison/cluster_output/rule-%j.err"\
+          --jobname "{jobid}.{rulename}"\
+          --use-conda\
+	  --latency-wait 90\
+          --keep-going\
+          --rerun-incomplete\
+	  --cores 160
