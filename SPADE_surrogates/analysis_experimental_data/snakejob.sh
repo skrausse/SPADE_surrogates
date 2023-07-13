3250a47ab10c1aa066d86095b578e238438a3b7e
Sven Krausse <s.krausse@fz-juelich.de>
2023-07-13 12:51:01 +0200

    took cluster running script from spade analysis

--- SPADE_surrogates/analysis_experimental_data/snakejob.sh
+++ SPADE_surrogates/analysis_experimental_data/snakejob.sh
@@ -1,30 +1,30 @@
-#!/bin/bash
-#SBATCH --time=96:00:00   								                                # walltime
-#SBATCH --ntasks=20   									                                # number of processor cores (i.e. tasks)
-#SBATCH --mem=84G	   								                                    # memory per CPU core
-#SBATCH --job-name "SPADE surrogates"                                                            # job name
-#SBATCH --mail-user=s.krausse@fz-juelich.de   						                    # email address
-#SBATCH --mail-type=END									                                # notify on job completed
-#SBATCH --mail-type=FAIL							                                    # notify on job failed
-#SBATCH --output=../../../01_cluster_output/surrogates_%j.out                           # redirect cluster output
-#SBATCH --error=../../../01_cluster_output/surrogates_%j.err                            # redirect cluster errors
-#SBATCH --partition=hamstein                                                            # Specify which partition to use
+#!/usr/bin/env bash
+#SBATCH --output=../../../01_cluster_output/surrogates_%j.out
+#SBATCH --error=../../../01_cluster_output/surrogates_%j.err
+#SBATCH --time=96:00:00
+#SBATCH --mem=32G
+#SBATCH --job-name=SPADE_analysis
+#SBATCH --mail-type=END,FAIL
+#SBATCH --partition=blaustein
 
+snakemake --unlock\
+	  --configfile configfile.yaml
+          --use-conda\
+          --cores 1
 
-snakemake --jobs 1000 \
-          --cluster "sbatch --ntasks 1 \
-                            --time 12:00:00 \
-                            --mail-type=FAIL \
-                            --mail-user=s.krausse@fz-juelich.de \
-                            --mem=32G \
-	                        --partition=hamstein \
-	                        -o ../../../01_cluster_output/surr_job_%j.out \
-                            -e ../../../01_cluster_output/surr_job_%j.err" \
-          --jobname "{jobid}.{rulename}" \
-	      --latency-wait 90 \
-          --keep-going \
-          --rerun-incomplete \
-          --nolock \
-	      --cores 160
+snakemake --jobs 1000\
+          --cluster-config cluster.json\
+          --cluster "sbatch -n {cluster.n}\
+                            --time {cluster.time}\
+                            --mail-type=FAIL\
+                            --mem={cluster.mem}\
+	                    --partition=blaustein\
+	                    -o /users/krausse/spade_comparison/cluster_output/rule-%j.out\
+                        -e /users/krausse/spade_comparison/cluster_output/rule-%j.err"\
+          --jobname "{jobid}.{rulename}"\
+          --use-conda\
+	  --latency-wait 90\
+          --keep-going\
+          --rerun-incomplete\
+	  --cores 160
 
-scontrol show jobid ${SLURM_JOBID} -dd        
