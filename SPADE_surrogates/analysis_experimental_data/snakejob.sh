ecc74c601580302bdd6c90ac8ebc41b01e97269d
Sven Krausse <s.krausse@fz-juelich.de>
2023-07-13 07:56:53 +0200

    batch submission script

--- SPADE_surrogates/analysis_experimental_data/snakejob.sh
+++ SPADE_surrogates/analysis_experimental_data/snakejob.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=96:00:00   								                                # walltime
-#SBATCH --ntasks=60   									                                # number of processor cores (i.e. tasks)
-#SBATCH --mem=32G	   								                                    # memory per CPU core
-#SBATCH -J SPADE surrogates"                                                            # job name
+#SBATCH --ntasks=20   									                                # number of processor cores (i.e. tasks)
+#SBATCH --mem=84G	   								                                    # memory per CPU core
+#SBATCH --job-name SPADE surrogates"                                                            # job name
 #SBATCH --mail-user=s.krausse@fz-juelich.de   						                    # email address
 #SBATCH --mail-type=END									                                # notify on job completed
 #SBATCH --mail-type=FAIL							                                    # notify on job failed
@@ -11,10 +11,20 @@
 #SBATCH --partition=hamstein                                                            # Specify which partition to use
 
 
-snakemake   --cores 60 \
-            --rerun-incomplete  \
-            --keep-going \
-            --latency-wait 90 \
-            --nolock \
+snakemake --jobs 1000 \
+          --cluster "sbatch --ntasks 1 \
+                            --time 12:00:00 \
+                            --mail-type=FAIL \
+                            --mail-user=s.krausse@fz-juelich.de \
+                            --mem={32G} \
+	                        --partition=hamstein \
+	                        -o ../../../01_cluster_output/surr_job_%j.out \
+                            -e ../../../01_cluster_output/surr_job_%j.err" \
+          --jobname "{jobid}.{rulename}" \
+	      --latency-wait 90 \
+          --keep-going \
+          --rerun-incomplete \
+          --nolock
+	      --cores 160
 
 scontrol show jobid ${SLURM_JOBID} -dd        
